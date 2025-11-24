61ab7c923ea67f7b270e3fdcf80f40a43ab52ab3
magneticstain <837837+magneticstain@users.noreply.github.com>
2025-11-23 21:25:01 -0500

    Deploy: Updates for release

--- app/app.go
+++ app/app.go
@@ -15,7 +15,7 @@ import (
 )
 
 const APP_ENV = "production"
-const APP_VER = "v3.1.4-rc2"
+const APP_VER = "v3.1.4-rc3"
 
 func GetSupportedPlatforms() []string {
 	return []string{"aws", "gcp", "azure"}
