3fee2e71456e21902ebcee65ee5b290f2063bb87
magneticstain <837837+magneticstain@users.noreply.github.com>
2025-11-26 22:35:38 -0500

    Deploy: Prep for release `v3.1.5`

--- app/app.go
+++ app/app.go
@@ -15,7 +15,7 @@ import (
 )
 
 const APP_ENV = "production"
-const APP_VER = "v3.1.5-rc2"
+const APP_VER = "v3.1.5"
 
 func GetSupportedPlatforms() []string {
 	return []string{"aws", "gcp", "azure"}
