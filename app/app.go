ff33feaf7d9a664eb83a1e541f6d7b43edc5d42b
magneticstain <837837+magneticstain@users.noreply.github.com>
2025-11-28 13:08:05 -0500

    Deploy: Update app env to `developement`

--- app/app.go
+++ app/app.go
@@ -14,7 +14,7 @@ import (
 	"github.com/magneticstain/ip-2-cloudresource/utils"
 )
 
-const APP_ENV = "production"
+const APP_ENV = "development"
 const APP_VER = "v3.2.0"
 
 func GetSupportedPlatforms() []string {
